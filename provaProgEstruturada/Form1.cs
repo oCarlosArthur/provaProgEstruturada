b7ebf6fccb2c7c597e12dbd9f075969cde985454
oCarlosArthur <carlos.almeida@sou.unifeb.edu.br>
2025-06-11 00:14:15 -0300

    Criei todos os arquivos .csv, que irei precisar para o programa. Criei uma variável com o caminho para chegar no arquivo usuarios.csv. Coloquei dentro da ação de clique do botão Entrar duas variáveis pegando os valores dos textboxs usuario e senha.

--- provaProgEstruturada/Form1.cs
+++ provaProgEstruturada/Form1.cs
@@ -12,6 +12,7 @@ namespace provaProgEstruturada
 {
     public partial class frmLogin : Form
     {
+        string usuariosCsv = "C:\\Users\\Usuario\\Desktop\\Faculdade\\provaProgEstruturada\\arquivosCsv\\usuarios.csv";
         public frmLogin()
         {
             InitializeComponent();
@@ -19,7 +20,8 @@ namespace provaProgEstruturada
 
         private void btnEntrar_Click(object sender, EventArgs e)
         {
-
+            string usuario = txtUsuario.Text;
+            string senha = txtSenha.Text;
         }
     }
 }
