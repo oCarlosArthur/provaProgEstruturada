6ceb1cad8a88f18a9cb38fd143045db874c89bbf
oCarlosArthur <carlos.almeida@sou.unifeb.edu.br>
2025-06-11 13:29:05 -0300

    Criei 3 funções que eu sei que vou usar, pois é o mesmo caso do cadastro de usuários: - Carregar clientes: Lê o csv e separa os dados com ";" depois adiciona na lista. - Atualizar lista: Limpa e coloca um item no listBox com o formato que eu decidi (CPF - Nome) - Salvar clientes: grava os dados da lista clientes no arquivo csv, um por linha, também separando os dados por ";".

--- provaProgEstruturada/frmCadClientes.cs
+++ provaProgEstruturada/frmCadClientes.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -17,6 +18,43 @@ namespace provaProgEstruturada
 
         string caminhoClientes = "C:\\Users\\Usuario\\Desktop\\Faculdade\\provaProgEstruturada\\arquivosCsv\\clientes.csv";
         List<string[]> clientes = new List<string[]>();
+
+        private void CarregarClientes()
+        {
+            clientes.Clear();
+            if (File.Exists(caminhoClientes))
+            {
+                string[] linhas = File.ReadAllLines(caminhoClientes);
+                foreach (string linha in linhas)
+                {
+                    string[] partes = linha.Split(';');
+                    if (partes.Length == 11)
+                    {
+                        clientes.Add(partes);
+                    }
+                }
+            }
+        }
+
+        private void AtualizarLista()
+        {
+            listClientes.Items.Clear();
+            foreach (string[] c in clientes)
+            {
+                listClientes.Items.Add(c[0] + " - " + c[1]); // CPF - Nome
+            }
+        }
+
+        private void SalvarClientes()
+        {
+            List<string> linhas = new List<string>();
+            foreach (var c in clientes)
+            {
+                linhas.Add(string.Join(";", c));
+            }
+            File.WriteAllLines(caminhoClientes, linhas);
+        }
+
         public frmCadClientes()
         {
             InitializeComponent();
