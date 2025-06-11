fe4bdd27417553f362c1c42e8640b807caa8d74c
oCarlosArthur <carlos.almeida@sou.unifeb.edu.br>
2025-06-11 13:20:51 -0300

    Fiz somente as preparações: um client que copiei do código que aprendemos na aula de como usar API externo, e esse client serve para fazer chamadas (como buscar cep). Fiz o caminho para o arquivo.csv. Fiz uma lista que guarda um array com os dados dos clientes (11 dados diferentes, pois tem 11 textBoxs).

--- provaProgEstruturada/frmCadClientes.cs
+++ provaProgEstruturada/frmCadClientes.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +13,10 @@ namespace provaProgEstruturada
 {
     public partial class frmCadClientes : Form
     {
+        private static readonly HttpClient client = new HttpClient();
+
+        string caminhoClientes = "C:\\Users\\Usuario\\Desktop\\Faculdade\\provaProgEstruturada\\arquivosCsv\\clientes.csv";
+        List<string[]> clientes = new List<string[]>();
         public frmCadClientes()
         {
             InitializeComponent();
