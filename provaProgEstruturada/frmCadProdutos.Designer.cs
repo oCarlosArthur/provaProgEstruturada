550bf1ac0d7197f59e125dc25ef566e24010090a
oCarlosArthur <carlos.almeida@sou.unifeb.edu.br>
2025-06-12 00:04:15 -0300

    Referenciei o caminho para o arquivo .csv de produtos. Além disso, criei uma lista(array) para os produtos.

--- provaProgEstruturada/frmCadProdutos.Designer.cs
+++ provaProgEstruturada/frmCadProdutos.Designer.cs
@@ -160,6 +160,7 @@
             this.Controls.Add(this.label1);
             this.Name = "frmCadProdutos";
             this.Text = "frmCadProdutos";
+            this.Load += new System.EventHandler(this.frmCadProdutos_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- provaProgEstruturada/frmCadProdutos.cs
+++ provaProgEstruturada/frmCadProdutos.cs
@@ -12,9 +12,17 @@ namespace provaProgEstruturada
 {
     public partial class frmCadProdutos : Form
     {
+        string caminhoProdutos = "C:\\Users\\Usuario\\Desktop\\Faculdade\\provaProgEstruturada\\arquivosCsv\\produtos.csv";
+        List<string[]> produtos = new List<string[]>();
+
         public frmCadProdutos()
         {
             InitializeComponent();
+        }
+
+        private void frmCadProdutos_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
