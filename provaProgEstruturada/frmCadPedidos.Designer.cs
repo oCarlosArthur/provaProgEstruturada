ec85735648ce64b28c5ac3f0e4f5224b717b2f38
oCarlosArthur <carlos.almeida@sou.unifeb.edu.br>
2025-06-12 01:30:21 -0300

    Criei o layout da telinha cadastrar pedidos com 3 textboxs, 4 botões, 1 comboBox e 1 listView.

--- provaProgEstruturada/frmCadPedidos.Designer.cs
+++ provaProgEstruturada/frmCadPedidos.Designer.cs
@@ -28,12 +28,194 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listItens = new System.Windows.Forms.ListView();
+            this.btnExcluirPedido = new System.Windows.Forms.Button();
+            this.btnSalvarPedido = new System.Windows.Forms.Button();
+            this.btnAdicionarItem = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbProduto = new System.Windows.Forms.ComboBox();
+            this.btnBuscarCliente = new System.Windows.Forms.Button();
+            this.lblNomeCliente = new System.Windows.Forms.Label();
+            this.txtQuantidade = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCodPedido = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCPF = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listItens
+            // 
+            this.listItens.HideSelection = false;
+            this.listItens.Location = new System.Drawing.Point(298, 24);
+            this.listItens.Name = "listItens";
+            this.listItens.Size = new System.Drawing.Size(440, 235);
+            this.listItens.TabIndex = 30;
+            this.listItens.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnExcluirPedido
+            // 
+            this.btnExcluirPedido.Location = new System.Drawing.Point(146, 210);
+            this.btnExcluirPedido.Name = "btnExcluirPedido";
+            this.btnExcluirPedido.Size = new System.Drawing.Size(122, 23);
+            this.btnExcluirPedido.TabIndex = 29;
+            this.btnExcluirPedido.Text = "Excluir Pedido";
+            this.btnExcluirPedido.UseVisualStyleBackColor = true;
+            // 
+            // btnSalvarPedido
+            // 
+            this.btnSalvarPedido.Location = new System.Drawing.Point(146, 181);
+            this.btnSalvarPedido.Name = "btnSalvarPedido";
+            this.btnSalvarPedido.Size = new System.Drawing.Size(122, 23);
+            this.btnSalvarPedido.TabIndex = 28;
+            this.btnSalvarPedido.Text = "Salvar Pedido";
+            this.btnSalvarPedido.UseVisualStyleBackColor = true;
+            // 
+            // btnAdicionarItem
+            // 
+            this.btnAdicionarItem.Location = new System.Drawing.Point(146, 152);
+            this.btnAdicionarItem.Name = "btnAdicionarItem";
+            this.btnAdicionarItem.Size = new System.Drawing.Size(122, 23);
+            this.btnAdicionarItem.TabIndex = 27;
+            this.btnAdicionarItem.Text = "Adicionar Item";
+            this.btnAdicionarItem.UseVisualStyleBackColor = true;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(143, 246);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(34, 13);
+            this.lblTotal.TabIndex = 26;
+            this.lblTotal.Text = "Total:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(143, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Produtos";
+            // 
+            // cmbProduto
+            // 
+            this.cmbProduto.FormattingEnabled = true;
+            this.cmbProduto.Location = new System.Drawing.Point(146, 84);
+            this.cmbProduto.Name = "cmbProduto";
+            this.cmbProduto.Size = new System.Drawing.Size(122, 21);
+            this.cmbProduto.TabIndex = 24;
+            // 
+            // btnBuscarCliente
+            // 
+            this.btnBuscarCliente.Location = new System.Drawing.Point(16, 84);
+            this.btnBuscarCliente.Name = "btnBuscarCliente";
+            this.btnBuscarCliente.Size = new System.Drawing.Size(100, 23);
+            this.btnBuscarCliente.TabIndex = 23;
+            this.btnBuscarCliente.Text = "Buscar Cliente";
+            this.btnBuscarCliente.UseVisualStyleBackColor = true;
+            // 
+            // lblNomeCliente
+            // 
+            this.lblNomeCliente.AutoSize = true;
+            this.lblNomeCliente.Location = new System.Drawing.Point(16, 68);
+            this.lblNomeCliente.Name = "lblNomeCliente";
+            this.lblNomeCliente.Size = new System.Drawing.Size(85, 13);
+            this.lblNomeCliente.TabIndex = 22;
+            this.lblNomeCliente.Text = "Nome do Cliente";
+            // 
+            // txtQuantidade
+            // 
+            this.txtQuantidade.Location = new System.Drawing.Point(146, 126);
+            this.txtQuantidade.Name = "txtQuantidade";
+            this.txtQuantidade.Size = new System.Drawing.Size(122, 20);
+            this.txtQuantidade.TabIndex = 21;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(143, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Quantidade";
+            // 
+            // txtCodPedido
+            // 
+            this.txtCodPedido.Location = new System.Drawing.Point(146, 41);
+            this.txtCodPedido.Name = "txtCodPedido";
+            this.txtCodPedido.Size = new System.Drawing.Size(122, 20);
+            this.txtCodPedido.TabIndex = 19;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(143, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Código do Pedido";
+            // 
+            // txtCPF
+            // 
+            this.txtCPF.Location = new System.Drawing.Point(16, 41);
+            this.txtCPF.Name = "txtCPF";
+            this.txtCPF.Size = new System.Drawing.Size(100, 20);
+            this.txtCPF.TabIndex = 17;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "CPF";
+            // 
+            // frmCadPedidos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 300);
+            this.Controls.Add(this.listItens);
+            this.Controls.Add(this.btnExcluirPedido);
+            this.Controls.Add(this.btnSalvarPedido);
+            this.Controls.Add(this.btnAdicionarItem);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cmbProduto);
+            this.Controls.Add(this.btnBuscarCliente);
+            this.Controls.Add(this.lblNomeCliente);
+            this.Controls.Add(this.txtQuantidade);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCodPedido);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCPF);
+            this.Controls.Add(this.label1);
+            this.Name = "frmCadPedidos";
             this.Text = "frmCadPedidos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listItens;
+        private System.Windows.Forms.Button btnExcluirPedido;
+        private System.Windows.Forms.Button btnSalvarPedido;
+        private System.Windows.Forms.Button btnAdicionarItem;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbProduto;
+        private System.Windows.Forms.Button btnBuscarCliente;
+        private System.Windows.Forms.Label lblNomeCliente;
+        private System.Windows.Forms.TextBox txtQuantidade;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCodPedido;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCPF;
+        private System.Windows.Forms.Label label1;
     }
 }
